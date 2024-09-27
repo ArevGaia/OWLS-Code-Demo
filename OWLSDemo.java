e97053813e01e84ba82395c0ed35b17e34e15277
ArevGaia <alimabou@oberlin.edu>
2024-09-26 21:01:48 -0400

    testing for code validity

--- OWLSDemo.java
+++ OWLSDemo.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+
 public class OWLSDemo{
     public static void main(String[] args) {
-        
+        int[] array = new int[3];
+        array[2] = 1;
+        ArrayList<String> groceryList = new ArrayList<>();
     }
 }
